0cfafc228c6dc5eafd37bd94274a3433dd7f2edf
rudresh <rudresh.kumbhare25@pccoepune.org>
2026-07-24 00:21:50 +0530

    Add Doubly Linked List header file with Node and List classes

--- linkedlist-library/DLL.h
+++ linkedlist-library/DLL.h
@@ -1,3 +1,30 @@
-#include <iostream>
-using namespace std;
+#ifndef DLL_H
+#define DLL_H
 
+class Node {
+public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int val);
+};
+
+class DLL {
+    Node* head;
+    Node* tail;
+
+public:
+    DLL();
+    ~DLL();
+
+    void push_front(int val);
+    void push_back(int val);
+    void insert(int val, int key);
+    void pop_front();
+    void pop_back();
+    void pop_specific(int val);
+    void displayDLL();
+};
+
+#endif
